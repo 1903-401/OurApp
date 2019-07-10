c3da94007ca374e3f648585f281387c9aa959f8a
Kesegofetse Rauwe <keserauwe@gmail.com>
2019-07-10 10:54:46 -0400

    nothing

--- TimerPart1/app/src/main/java/com/example/timerpart1/GameChoicesActivity.java
+++ TimerPart1/app/src/main/java/com/example/timerpart1/GameChoicesActivity.java
@@ -7,16 +7,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class GameChoicesActivity extends AppCompatActivity {
 
     private Button one;
     private Button two;
     private Button three;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_choices);
+
+
 
         one = findViewById(R.id.games_one);
         two = findViewById(R.id.games_two);
